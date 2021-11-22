b950b48d84baea504f46b3cf6dd1bf5dee2bef52
gyeong <deborahoh94@gmail.com>
2021-11-22 16:01:47 +0900

    init

---
+++ app/src/main/java/com/gyeong/myapplication/PermissionLifecycleObserver.kt
@@ -0,0 +1,12 @@
+package com.gyeong.myapplication
+
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.LifecycleOwner
+
+class PermissionLifecycleObserver : DefaultLifecycleObserver {
+
+    override fun onCreate(owner: LifecycleOwner) {
+        super.onCreate(owner)
+
+    }
+}
